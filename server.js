0b77ea05b661db6b1904e05293824f9a38ca3ef0
destinyleia <destinylindgrenc@gmail.com>
2021-02-08 13:32:50 +0100

    backend up to date

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+import imagesGameData from './data/images-game.json'
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/music-game'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Image = mongoose.model('Image', {
+  image: String
+})
+
+if (process.env.RESET_DB) {
+  const fixedDatabase = async () => {
+    await Image.deleteMany()
+    imagesGameData.forEach((item) => {
+      const newImage = new Image(item)
+      newImage.save()
+    })
+  }
+  fixedDatabase()
+}
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
@@ -12,10 +34,19 @@ const app = express()
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
-
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello welcome to my music game - image api!')
+})
+
+app.get('/images', async (req, res) => {
+  const getImages = await Image.find(req.query)
+
+  if (getImages) {
+    res.json(getImages)
+  } else {
+    res.status(404).json({ error: 'There is no image like this' })
+  }
 })
 
 // Start the server
